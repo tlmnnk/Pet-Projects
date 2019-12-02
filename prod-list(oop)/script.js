06df6408080dcfc953dd1db4ab3be94d452de1a7
vladislav t <vt118@mail.ru>
2019-12-03 00:41:14 +0300

    start prod list

--- prod-list(oop)/script.js
+++ prod-list(oop)/script.js
@@ -2,55 +2,74 @@
 
 
 class Product {
-    title = 'DEFAULT';
-    imageUrl;
-    description;
-    price;
-
+    // title = 'DEFAULT';
+    // imageUrl;
+    // description;
+    // price;
     constructor(title, image, desc, price) {
         this.title = title;
-        this.image = image;
+        this.imageUrl = image;
         this.description = desc;
         this.price = price;
     }
 }
-const productList = {
-    products: [
+
+class ProductItem {
+     
+    constructor(product) {
+        this.product = product;
+
+    }
+
+    render() {
+        const prodEl = document.createElement('li');
+          prodEl.className = 'product-item';
+          prodEl.innerHTML = `
+          <div>
+              <img src="${this.product.imageUrl}" alt="${this.product.title}">
+              <div class='product-item__content'>
+                  <h2>${this.product.title}</h2>
+                  <h3>\$${this.product.price}</h3>
+                  <p>${this.product.description}</p>
+                  <button>Add to Cart</button>
+              </div>
+              </div>
+          `;
+          return prodEl;
+    }
+}
+
+
+class ProductList {
+    products = [
         new Product(
         'A sofa',
          'https://m.media-amazon.com/images/I/91VMNtOVYpL._SR400,500_.jpg',
-         143.99,
-        'Cosy living room sofa'
+        'Cosy living room sofa',
+        143.99
         ),
         new Product(
         'A carpet',
         'https://images-eu.ssl-images-amazon.com/images/G/31/acs/amazon-designer/2017/07/28/DURM-56D2B2BA5E4C72EJ.jpeg',
-        89.99,
-        'A nice looking carpet'
+        'A nice looking carpet',
+        89.99
         )
-    ],
-    render() {  
-          const renderHook = document.getElementById('app');
-          const prodList = document.createElement('ul');
-          prodList.className = 'product-list';
-          for(const prod of this.products) {
-            const prodEl = document.createElement('li');
-            prodEl.className = 'product-item';
-            prodEl.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.title}">
-                <div class='product-item__content'>
-                    <h2>${prod.title}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>
-                </div>
-                </div>
-            `;
-            prodList.append(prodEl);
-          }
-          renderHook.append(prodList);
-    }
-};
+    ];
 
+    constructor() {}
+
+    render() {  
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for(const prod of this.products) {
+          const product = new ProductItem(prod);
+          const prodEl = product.render();
+          prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
+  }
+}
+
+const productList = new ProductList();
 productList.render();
