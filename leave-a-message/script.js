f6a08a6188887319d05bd2fe0c3e141d85860256
Vladislav T <vt118@mail.ru>
2019-12-10 14:58:13 +0300

    added localstorage for leave-a-message

--- leave-a-message/script.js
+++ leave-a-message/script.js
@@ -2,12 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     let input  = document.querySelector('.promo__input');
-    let message = document.querySelector('.promo__message')
+    let message = document.querySelector('.promo__message');
     const button = document.querySelector('.promo__send');
+    message.innerHTML = localStorage.getItem('lastMessage');
 
 
     button.addEventListener('click', function() {
       message.innerHTML = input.value;
+      localStorage.setItem('lastMessage', input.value);
       input.value = '';
     });
    
