2abef81890291930ca4afad8df48e25f3fc82555
vladislav t <vt118@mail.ru>
2019-12-30 23:51:39 +0300

    playground myHttpRequest

--- playground/script.js
+++ playground/script.js
@@ -1,27 +1,93 @@
 //jshint esversion: 6
 
-const p = new Promise((resolve, reject) => {
-    setTimeout( () => {
-        console.log('Preparing data....');
+// const p = new Promise((resolve, reject) => {
+//     setTimeout( () => {
+//         console.log('Preparing data....');
         
-        setTimeout(() => {
-            console.log('Sending data...');
-            const someObj = {
-                name: 'someData',
-                type: 'text'
-            };
-            resolve(someObj);        
-        }, 2000);
+//         setTimeout(() => {
+//             console.log('Sending data...');
+//             const someObj = {
+//                 name: 'someData',
+//                 type: 'text'
+//             };
+//             resolve(someObj);        
+//         }, 2000);
 
 
-    },2000);
-});
+//     },2000);
+// });
 
-p.then(data => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            data.isReceived = true;
-            resolve(data);
-        }, 2000);
+// p.then(data => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             data.isReceived = true;
+//             resolve(data);
+//         }, 2000);
+//     });
+// }).then(clientData => console.log(clientData));
+
+
+function getData(cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+    
+    xhr.addEventListener('load', () => {
+       cb(xhr.responseText);
     });
-}).then(clientData => console.log(clientData));
+    
+    xhr.send();
+    }
+    
+    
+function createPost(body, cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://jsonplaceholder.typicode.com/posts');
+    xhr.addEventListener('load', ()=>{
+        const response = JSON.parse(xhr.responseText);
+        cb(response);
+    });
+    
+    xhr.setRequestHeader('Content-type', 'application/json; charset=UTF-8');
+        
+    xhr.send(JSON.stringify(body));
+    }
+
+createPost({name:'John', age: 20}, data => console.log(data));
+
+
+function myHttpRequest({method, url} = {}, cb) {
+    try {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        
+        xhr.addEventListener('load', () => {
+            if (Math.floor(xhr.status / 100) !== 2) {
+                cb('Error. Status code: ' + xhr.status, xhr);
+                return;
+            }
+            const response = JSON.parse(xhr.responseText);
+           cb(null, response);
+        });
+    
+        xhr.addEventListener('error', () => {
+            cb('Error. Status code: ' + xhr.status, xhr);
+        });   
+        xhr.send();
+    } catch (error) {
+        cb(error);
+    }
+   
+}
+
+myHttpRequest(
+    { 
+        method: 'GET', 
+        url: 'https://jsonplaceholder.typicode.com/posts'
+    }, 
+    (err, res) => {
+    if(err) {
+        console.log(err);
+        return;
+    }
+    console.log(res);
+});
