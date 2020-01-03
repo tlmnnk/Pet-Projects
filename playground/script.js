2d23ae472617e5b3bbb8842de709e4f0cd4db8b7
vladislav t <vt118@mail.ru>
2020-01-04 00:09:31 +0300

    created handy XmlhttpRequest wrapper

--- playground/script.js
+++ playground/script.js
@@ -52,8 +52,6 @@ function createPost(body, cb) {
     xhr.send(JSON.stringify(body));
     }
 
-createPost({name:'John', age: 20}, data => console.log(data));
-
 
 function myHttpRequest({method, url} = {}, cb) {
     try {
@@ -79,15 +77,85 @@ function myHttpRequest({method, url} = {}, cb) {
    
 }
 
-myHttpRequest(
-    { 
-        method: 'GET', 
-        url: 'https://jsonplaceholder.typicode.com/posts'
-    }, 
+// myHttpRequest(
+//     { 
+//         method: 'GET', 
+//         url: 'https://jsonplaceholder.typicode.com/posts'
+//     }, 
+//     (err, res) => {
+//     if(err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(res);
+// });
+
+
+function http() {
+    return {
+        get(url, cb) {
+            try {
+                const xhr = new XMLHttpRequest();
+                xhr.open('GET', url);
+                
+                xhr.addEventListener('load', () => {
+                    if (Math.floor(xhr.status / 100) !== 2) {
+                        cb('Error. Status code: ' + xhr.status, xhr);
+                        return;
+                    }
+                    const response = JSON.parse(xhr.responseText);
+                   cb(null, response);
+                });
+            
+                xhr.addEventListener('error', () => {
+                    cb('Error. Status code: ' + xhr.status, xhr);
+                });   
+                xhr.send();
+            } catch (error) {
+                cb(error);
+            }
+        },
+        post(url, body, headers, cb) {
+            try {
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', url);
+
+                //;
+                if(headers) {
+                    Object.entries(headers).forEach(([key, value]) => { 
+                        xhr.setRequestHeader(key, value);
+                    });
+                }
+                xhr.addEventListener('load', () => {
+                    if (Math.floor(xhr.status / 100) !== 2) {
+                        cb('Error. Status code: ' + xhr.status, xhr);
+                        return;
+                    }
+                    const response = JSON.parse(xhr.responseText);
+                   cb(null, response);
+                });
+            
+                xhr.addEventListener('error', () => {
+                    cb('Error. Status code: ' + xhr.status, xhr);
+                });   
+                xhr.send(JSON.stringify(body));
+            } catch (error) {
+                cb(error);
+            }
+        }
+    };
+}
+
+const myHttp = http();
+
+myHttp.post(
+    'https://jsonplaceholder.typicode.com/posts',
+    {name:'John', age: 20},
+    {'Content-Type': 'application/json'},
     (err, res) => {
-    if(err) {
-        console.log(err);
-        return;
-    }
-    console.log(res);
-});
+        if(err) {
+            console.log(err); return;
+        }
+        console.log(res);
+    } 
+);
