457057e7055552f4f88f181b7ad1a83e8150998b
Vladislav T <vt118@mail.ru>
2020-01-22 13:53:35 +0300

    added materialized to html

---
+++ avia-tickets/src/js/plugins/materialize.js
@@ -0,0 +1,31 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+//init select
+
+const select = document.querySelector('select');
+M.FormSelect.init(select);
+
+
+export function getSelectInstance(elem) {
+    return M.FormSelect.getInstance(elem);
+}
+
+//init Autocomplete
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete);
+
+export function getAutocompleteInstance(elem) {
+    return M.Autocomplete.getInstance(elem);
+}
+
+//init datepickers
+
+const datepickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepickers, {
+    showClearBtn: true
+});
+
+export function getDatepickerInstance(elem) {
+    return M.Datepicker.getInstance(elem);
+}
--- avia-tickets/webpack.config.js
+++ avia-tickets/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const precss = require('precss');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // Итак,  чтобы вебпак начал свою работу, нужно указать главный (основной) файл, который будет включать в себя все другие необходимые файлы (модули).
@@ -39,7 +40,7 @@ module.exports = {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader',
+            loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: 'css-loader',
@@ -74,6 +75,7 @@ module.exports = {
   //Например, плагин для минификации кода (во время сборки код подвергается очистке и минификации).
   //Или плагин для сборки html страницы и css кода (скрипты вставляются в html, куски css собираются в один файл).
   plugins: [
+    new MiniCssExtractPlugin({filename: './style.css'}),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
